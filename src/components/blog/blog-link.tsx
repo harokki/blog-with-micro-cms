a8ad1de698fc8f0ec6dadc02d39ec7bf5e909bc1
hiroki_iizuka <alljapan.ma.kabu@gmail.com>
2021-05-24 14:30:08 +0900

    zennブログの場合はタイトルの最後にzennのIconをつける

--- src/components/blog/blog-link.tsx
+++ src/components/blog/blog-link.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
+import { Zenn } from '@icons-pack/react-simple-icons';
 
 import { BlogResponse } from 'domains/microCMS/models/blog';
 import styles from './index.module.css';
@@ -8,18 +9,31 @@ type P = {
   blog: BlogResponse;
 };
 
+const getIcon = (hostname: string) => {
+  if (hostname === 'zenn.dev') {
+    return <Zenn color="#3EA8FF" size={18} />;
+  }
+
+  return null;
+};
+
 export const BlogLink: React.FC<P> = ({ blog }) => {
   if (blog.externalUrl) {
+    const { hostname } = new URL(blog.externalUrl);
+
     return (
-      <Link href={blog.externalUrl}>
-        <a
-          className={styles.blogLink}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {blog.title}
-        </a>
-      </Link>
+      <>
+        <Link href={blog.externalUrl}>
+          <a
+            className={styles.blogLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {blog.title}
+          </a>
+        </Link>
+        {getIcon(hostname)}
+      </>
     );
   }
 
